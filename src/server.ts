9bb9b04a991e52caf930ce07fc5da865d17e2d67
Susanto Mandal <susantomandal1998@gmail.com>
2025-04-29 21:07:24 +0530

    fix: update default server port from 3000 to 5000

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import PostController from './controllers/post.contoller';
 import LoadPostsData from './utils/LoadPostsData';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
